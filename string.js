d580299598a7f04936885a28cf9e22613b9a7396
Fahad Oni <fahadoni272@gmail.com>
2026-07-24 20:32:18 +0600

    completed q-1.2

--- string.js
+++ string.js
@@ -1,3 +1,4 @@
+// task-1.1
 let str = 'hello'
 let arr = ['h', 'e', 'l', 'l', 'o'];
 
@@ -8,3 +9,16 @@ console.log(str[0]);
 console.log(arr[0]);
 
 // console.log(str.push('!'));
+
+// task-1.2
+let string = 'JavaScript';
+
+let array = string.split("");
+
+console.log(array);
+
+let strArray = ['c', 'a', 't'];
+
+let arrayStr = strArray.join('')
+
+console.log(arrayStr);
